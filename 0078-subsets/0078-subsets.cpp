3dd6dfb5ee04345ad6e9874ad9cf45158cd9a196
Piyush Yadav <piyushyxd@gmail.com>
2026-08-25 15:08:47 +0530

    Time: 0 ms (100%), Space: 9.8 MB (96.39%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // Using power set
+        int n=nums.size();
+        int subset=1<<n;
+        vector<vector<int>> ans;
+        for(int num=0;num<subset;num++){
+            vector<int>list;
+            for(int i=0;i<n;i++){
+                if(num&(1<<i)){
+                    list.push_back(nums[i]);
+                }
+            }
+            ans.push_back(list);
+
+        } return ans;
+
+        
+    }
+};
