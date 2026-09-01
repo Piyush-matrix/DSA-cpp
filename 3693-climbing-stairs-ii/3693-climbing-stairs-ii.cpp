c0b627a69a8ae6993cf50bfb4675fdda73b3f4c4
Piyush Yadav <piyushyxd@gmail.com>
2026-09-02 00:39:43 +0530

    Time: 19 ms (57.56%), Space: 177.9 MB (45.45%) - LeetHub

---
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int climbStairs(int n, vector<int>& costs) {
+        vector<int> dp(n + 1, 0);
+
+        dp[1] = costs[0] + 1;
+
+        if (n > 1) {
+            dp[2] = costs[1] + min({dp[1] + 1, dp[0] + 4});
+        }
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = costs[i - 1] + min({
+                dp[i - 1] + 1,
+                dp[i - 2] + 4,
+                dp[i - 3] + 9
+            });
+        }
+
+        return dp[n];
+    }
+};
