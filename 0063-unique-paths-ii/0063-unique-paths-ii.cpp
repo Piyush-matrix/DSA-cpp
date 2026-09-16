29f5044caeb4a4743e4f463fca49566914bf4e86
Piyush Yadav <piyushyxd@gmail.com>
2026-09-17 02:20:14 +0530

    Time: 2 ms (12.64%), Space: 12 MB (6.54%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,7 +5,6 @@ public:
         int n=obstacleGrid[0].size();
         vector<vector<int>> dp(m+1,vector<int>(n+1,0));
         if(obstacleGrid[0][0]==1) return 0;
-        dp[0][0]=1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(obstacleGrid[i][j]==1){
